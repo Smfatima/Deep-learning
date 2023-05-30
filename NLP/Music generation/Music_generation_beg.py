b764ea8199db9b38821bd386e8b1d4ecbce9d8a4
Maryam Fatima <s.m.fatima13@gmail.com>
2023-05-30 09:54:06 +0500

    Delete NLP/Music generation directory

--- NLP/Music generation/Music_generation_beg.py
+++
@@ -1,8 +0,0 @@
-# Automated music generation using RNNs - this is beginner to advance script for my understanding of music generation to the production of music
-
-
-# Import Libraries
-import pandas as pd
-import numpy as np
-import matplotlib as plt
-
